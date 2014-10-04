39389dc0fde2b9d62afe2324bef32b75b7191eef
Dharmender Sharma <iamdharm20@gmail.com>
2014-10-04 15:15:00 +0530

    My first kernel module
    Signed-off-by: Dharmender Sharma <iamdharm20@gmail.com>

---
+++ kernel_basic.c
@@ -0,0 +1,21 @@
+/*  
+ *  kernel_basic.c - Demonstrating the module_init() and module_exit() macros.
+ *  This is preferred over using init_module() and cleanup_module().
+ */
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/init.h>		/* Needed for the macros */
+
+static int __init kernel_hello_init(void)
+{
+	printk(KERN_INFO "Hello, kernel \n");
+	return 0;
+}
+
+static void __exit kernel_hello_exit(void)
+{
+	printk(KERN_INFO "Goodbye, kernel\n");
+}
+
+module_init(kernel_hello_init);
+module_exit(kernel_hello_exit);
