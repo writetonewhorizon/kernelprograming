aa07e0f6bcf5727c981a5aabcb67f98198ba4928
iamdharm20 <iamdharm20@gmail.com>
2015-05-11 14:57:45 +0530

    Update kernel_basic.c

--- kernel_basic.c
+++ kernel_basic.c
@@ -1,4 +1,10 @@
-/*  
+/*  [1]: http://williamstallings.com/OS/
+[2]: http://blog.rlove.org/2010/07/li...
+[3]: http://lwn.net/Kernel/LDD3/
+[4]: http://elinuxdd.com/
+[5]: https://github.com/martinezjavie...
+[6]: http://lwn.net/Articles/416690/
+
  *  kernel_basic.c - Demonstrating the module_init() and module_exit() macros.
  *  This is preferred over using init_module() and cleanup_module().
  */
